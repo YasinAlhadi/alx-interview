4783db20b9ebe27b4f7850b539b59e02ceb0f2eb
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-04-20 08:55:03 +0200

    Pascal Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    """Pascal Triangle"""
+    if (n <= 0):
+        return []
+    for i in range(n):
+        print(' '*(n-i), end='')
+        print(' '.join(map(str, str(11**i))))
