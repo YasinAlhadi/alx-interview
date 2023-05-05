f5af5c9ec42b4b424229a63adee783d6f2c4b3bb
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-05-05 04:44:07 +0300

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Minimum Operations"""
+
+
+def minOperations(n):
+    """Given a number n, write a method that calculates the fewest number of
+    operations needed to result in exactly n H characters in the file."""
+    if n <= 1:
+        return 0
+    i = 2
+    count = 0
+    while i <= n:
+        if n % i == 0:
+            count += i
+            n = n / i
+        else:
+            i += 1
+    return count
