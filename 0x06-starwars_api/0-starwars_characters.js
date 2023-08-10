f33ce5881fefe226a94c2f762a93fb991f1a233a
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-08-10 14:41:57 +0400

    Starwars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + '/';
+const url = 'https://swapi-api.alx-tools.com/api';
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      request(character, function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
+if (process.argv.length > 2) {
+  request(`${url}/films/${process.argv[2]}/`, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      const characters = JSON.parse(body).characters;
+      for (const character of characters) {
+        request(character, function (error, response, body) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log(JSON.parse(body).name);
+          }
+        });
+      }
     }
-  }
-});
+  });
+}
